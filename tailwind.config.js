2b6bbf6528bbc072f4008705c6e2028914fdb611
vannakv405@gmail.com <vannakv405@gmail.com>
2024-10-26 23:26:01 +0700

    Check file tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        moulpali: ['Moulpali_400Regular', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
